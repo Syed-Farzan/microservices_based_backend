a578b488aba118677be6c8eb50c4b3beb3f0bc32
Syed Arhan <arhannaqshbandi@gmail.com>
2026-09-10 10:23:34 +0530

    feat: implement Notification Service to asynchronously consume NATS events

---
+++ notification_service/main.py
@@ -0,0 +1,45 @@
+import asyncio
+import os
+import json
+import nats
+
+NATS_URL = os.getenv("NATS_URL", "nats://nats:4222")
+
+
+async def message_handler(msg):
+    try:
+        data = json.loads(msg.data.decode())
+        email = data.get("email")
+
+        print(
+            f"[{msg.subject}] Asynchronously sending welcome email to: {email}...",
+            flush=True,
+        )
+
+        await msg.ack()
+    except Exception as e:
+        print(f"Error processing message: {e}", flush=True)
+
+        await msg.nak()
+
+
+async def main():
+    print("Connecting to NATS...", flush=True)
+    nc = await nats.connect(NATS_URL)
+    js = nc.jetstream()
+
+    await js.subscribe(
+        "user.created", cb=message_handler, durable="notification_service_durable"
+    )
+    print("Notification Service active and listening...", flush=True)
+
+    try:
+
+        while True:
+            await asyncio.sleep(1)
+    finally:
+        await nc.drain()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
